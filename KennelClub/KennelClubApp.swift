a36cd7052b04a5364ebc9e6a46bfe36734f1be6d
KaMaDi <artkamadi@gmail.com>
2024-05-31 09:37:41 +0300

    Initial Commit

---
+++ KennelClub/KennelClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  KennelClubApp.swift
+//  KennelClub
+//
+//  Created by Jedi on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct KennelClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
